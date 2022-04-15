99142e1ae369c6e6a34760bcfdc223bb433c5882
Enol <905691948@qq.com>
2022-04-15 20:15:19 +0800

    950-2：水果射手戒网瘾版 927：【新增】收集蜂蜜

---
+++ game/927/src/settings.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default","UI"],collisionMatrix:[[true],[false,false]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/MainGame.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"6c0a3",resources:"24264",main:"9b5b8"}};
